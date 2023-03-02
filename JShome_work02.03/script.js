40e7eaf70512a0b147498f0b832f4935f78d9276
aanyamarija <124683108+aanyamarija@users.noreply.github.com>
2023-03-02 19:49:49 +0100

    Add files via upload
    Были вопросы со вторым пунктом.

---
+++ JShome_work02.03/script.js
@@ -0,0 +1,64 @@
+
+//1
+/*
+const numbers = [0,1,2,3,4,5,6,7,8,9,10]
+for (let i =0; i < numbers.length;i++) {
+    if (numbers[i] % 2 == 1)
+    {
+        console.log(numbers[i])
+    }
+}
+*/
+//2 ?
+
+/*
+const numbers = [ 10,15,20,25,30,43,50,55,70]
+for (let i =0; i < numbers.length;i++) {
+    if ( numbers[i] > 20,  numbers[i] < 55)
+    {
+        console.log(numbers[i])
+    }
+
+} 
+*/
+
+//3
+
+/*const numbers = [3,5,11,2,8,1,6]
+console.log(numbers)
+*/
+
+//4
+
+/*
+const numbers = [3,5,11,2,8,1,6]
+const numbers_squared =[]
+ const square1 = [ numbers[0] * numbers[0]]
+ const square2 = [ numbers[1] * numbers[1]]
+ const square3 = [ numbers[2] * numbers[2]]
+ const square4 = [numbers[3] * numbers[3]]
+ const square5 = [ numbers[4] * numbers[4]]
+ const square6 = [ numbers[5] * numbers[5]]
+ const square7 = [ numbers[6] * numbers[6]]
+
+ numbers_squared.push(square1,square2,square3,square4,square5,square6,square7)
+ console.log(numbers_squared)
+
+ //5
+
+ const last_elem = [ numbers_squared[6]]
+ console.log(last_elem)
+ */
+
+//6
+
+/*
+const user = {
+    first_name: 'Ivan',
+    last_name: 'Ivanov',
+    age: 20,
+    salary: 500
+}
+console.log(`User's name is ${user.first_name} ${user.last_name}. He is ${user.age} years old`)
+*/
+
